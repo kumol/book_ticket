db1512de1718f6de831879542362387ca799668e
kumol <kumol01763@gmail.com>
2019-12-25 02:19:44 +0600

    model

--- models/bus.js
+++ models/bus.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const busSchema = mongoose.Schema({
-    seat: [{ type: mongoose.Schema.Types.ObjectId, ref: "Ticket" , default:"5dd453128c930e4846b3a077", required:true}],
+    seat: { type : String },
     date : {type : Date },
     time : { type : Date },
     image : { type : String }
--- models/seat.js
+++ models/seat.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const ticketSchema = mongoose.Schema({
-
+    bus : {type : String },
     name : {type : String },
     number :{type : String },
     count : {type : Number },
-    status : {type : Boolean}
+    status : {type : Boolean},
+    displayName : {type : String },
+    passenger : {type :String}
 });
 module.exports = mongoose.model("Ticket",ticketSchema);
