572e36151c6d4bac9344034caf67f3371386456f
kumol <kumol01763@gmail.com>
2019-12-26 22:50:36 +0600

    done

--- models/bus.js
+++ models/bus.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const busSchema = mongoose.Schema({
-    seat: { type : String },
+    seat: [{ type : String }],
     date : {type : Date },
     time : { type : Date },
-    image : { type : String }
+    image : { type : String },
+    isBooked : Boolean
 });
 module.exports = mongoose.model("Bus",busSchema);
